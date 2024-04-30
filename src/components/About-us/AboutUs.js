be34d51aeb542863146cf0a6e6dd63a437ecbf7d
omar-vtc <72626558+omar-vtc@users.noreply.github.com>
2024-04-30 18:02:05 +0300

    strategy- semi done
    remaining to split components

--- src/components/About-us/AboutUs.js
+++ src/components/About-us/AboutUs.js
@@ -1,41 +1,17 @@
-// import "../../css/index.css";
 import "../../css/aboutUs.css";
 import AboutFuture from "./AboutFuture";
 import strategy from "../../img/strategy.png";
+
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import ImageMap from "./ImageMap";
-function AboutUs() {
-  const history = useHistory();
+import Strategy from "./Strategy";
 
-  const handleAreaClick = (areaId) => {
-    // Handle click events for map areas
-    console.log("Clicked area:", areaId);
-    // Navigate to the corresponding page
-    history.push("/consulting");
-    // switch (areaId) {
-    //   case "northAmerica":
-    //     history.push("/north-america");
-    //     break;
-    //   case "europe":
-    //     history.push("/europe");
-    //     break;
-    //   // Add more cases for other areas if needed
-    //   default:
-    //     break;
-    // }
-  };
+function AboutUs() {
   return (
     <div>
       <AboutFuture />
-      <div className="strategy">
-        <div className="strategy-title">
-          <div className="float-right">
-            <h4>معلومات عن</h4>
-            <h1>استراتجية اكاديمية المستقبل</h1>
-          </div>
-        </div>
-      </div>
+      <Strategy />
       <ImageMap />
     </div>
   );
