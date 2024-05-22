251af42affdcfcaa00148eff2d1c06dbf7f9cbcd
omar-vtc <72626558+omar-vtc@users.noreply.github.com>
2024-05-22 23:01:25 +0300

    consulting is done

--- src/components/About-us/Services.js
+++ src/components/About-us/Services.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import React from "react";
 
 export const Services = (props) => {
@@ -5,7 +6,7 @@ export const Services = (props) => {
     <div id="services" className="text-center">
       <div className="container">
         <div className="section-title">
-          <h2>Our Services</h2>
+          <h2>مجالات اكادمية المستقبل</h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
             dapibus leonec.
@@ -21,13 +22,30 @@ export const Services = (props) => {
         >
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-4">
-                  {" "}
-                  <i className={d.icon}></i>
-                  <div className="service-desc">
-                    <h3>{d.name}</h3>
-                    <p>{d.text}</p>
-                  </div>
+                <div
+                  key={`${d.name}-${i}`}
+                  className="col-md-4 hovering"
+                  style={{
+                    height: "370px",
+                    // border: "solid white",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyItems: "center",
+                  }}
+                >
+                  <Link
+                    style={{
+                      textDecoration: "none",
+                      color: "white",
+                    }}
+                    to={d.to}
+                  >
+                    <i className={d.icon}></i>
+                    <div className="service-desc">
+                      <h3>{d.name}</h3>
+                      <p>{d.text}</p>
+                    </div>
+                  </Link>
                 </div>
               ))
             : "loading"}
--- src/pages/Consulting.js
+++ src/pages/Consulting.js
@@ -4,12 +4,17 @@ import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import IntroBoxesCont from "../components/Content/IntroBoxes/IntroBoxesCont";
 import StrSpTitle from "../components/UtilitiesCont/StrSpTitle";
+import { NewH } from "../components/NewHeader/NewH";
+import { ConsultServices } from "../components/Consulting/ConsultServices";
+import NewFooter from "../components/Footer/NewFooter";
 
 function Consulting() {
   return (
     <div>
-      <Header homePage={false} content={IntroBoxesCont[7]} />
-      <div className="consult">
+      {/* <Header homePage={false} content={IntroBoxesCont[7]} /> */}
+      <NewH />
+      <ConsultServices />
+      {/* <div className="consult">
         <StrSpTitle
           className="strategy-title-consult"
           subtitle="معلومات عن"
@@ -22,7 +27,8 @@ function Consulting() {
       </div>
       <div className="marginTop">
         <Footer />
-      </div>
+      </div> */}
+      <NewFooter />
     </div>
   );
 }
